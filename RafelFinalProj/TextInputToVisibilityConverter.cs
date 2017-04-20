f8cfff57bc8be5522c2ca982bdf6b70e7c18ca75
Nir <nirturk@gmail.com>
2017-04-20 21:36:46 +0300

    Update to the notifications box and some bugs in text convert

--- RafelFinalProj/TextInputToVisibilityConverter.cs
+++ RafelFinalProj/TextInputToVisibilityConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,28 +9,25 @@ using System.Windows.Data;
 
 namespace RafelFinalProj
 {
-    class TextInputToVisibilityConverter
-    {
-		 class TextInputToVisibilityConverter : IMultiValueConverter
-		{
-			public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-			{
-				// Always test MultiValueConverter inputs for non-null 
-				// (to avoid crash bugs for views in the designer) 
-				if (values[0] is bool && values[1] is bool)
-				{
-					bool hasText = !(bool)values[0];
-					bool hasFocus = (bool)values[1];
-					if (hasFocus || hasText)
-						return Visibility.Collapsed;
-				}
-				return Visibility.Visible;
-			}
+class TextInputToVisibilityConverter : IMultiValueConverter
+		{      
+            public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+			    {
+				    // Always test MultiValueConverter inputs for non-null 
+				    // (to avoid crash bugs for views in the designer) 
+				    if (values[0] is bool && values[1] is bool)
+				    {
+					    bool hasText = !(bool)values[0];
+					    bool hasFocus = (bool)values[1];
+					    if (hasFocus || hasText)
+						    return Visibility.Collapsed;
+				    }
+				    return Visibility.Visible;
+			    }
 
-			public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-			{
-				throw new NotImplementedException();
-			}
+            public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }    
 		}
-    }
 }
